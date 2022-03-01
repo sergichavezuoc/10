800ed197a9e4573c7b2b11f7b3c9fee223b2dd02
Sergi Chavez <schavezb@uoc.edu>
2022-03-01 17:39:12 +0100

    Version con listado

--- src/app/cancion/cancion.component.ts
+++ src/app/cancion/cancion.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Cancion } from "../cancion";
+import { CANCIONES } from "../mock-canciones";
 @Component({
   selector: 'app-cancion',
   templateUrl: './cancion.component.html',
   styleUrls: ['./cancion.component.css']
 })
 export class CancionComponent implements OnInit {
+  canciones = CANCIONES;
   cancion: Cancion = {
     id: 1,
     autor: "Los Ronaldos",
---
+++ src/app/mock-canciones.ts
@@ -0,0 +1,10 @@
+import { Cancion } from './cancion';
+
+export const CANCIONES: Cancion[] = [
+    { id: 1, autor: "Los Ronaldos", titulo: "Idiota", album: "Primer Album", anyo: 2005, genero: "Pop" },
+    { id: 2, autor: "Iván Ferreiro", titulo: "Turnedo", album: "Todo Ivan", anyo: 2014, genero: "Pop" },
+    { id: 3, autor: "Los Rodríguez", titulo: "Sin documentos", album: "Hasta luego", anyo: 2002, genero: "Rock" },
+    { id: 4, autor: "El Último de la Fila", titulo: "Insurrección", album: "Enemigos de lo ajeno", anyo: 2000, genero: "Canción Autor" },
+    { id: 5, autor: "Love of Lesbian", titulo: "Niña imantada", album: "Cuentos chinos para niños del Japón", anyo: 2010, genero: "Pop" },
+    { id: 6, autor: "Los Piratas", titulo: "Años 80", album: "Disco duro", anyo: 2007, genero: "Heavy" },
+];
